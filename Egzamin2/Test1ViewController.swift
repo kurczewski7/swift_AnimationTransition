c947bcc2360b860b18932fef8e023247baee9fb0
kurczewski7 <slawomir.kurczewski@gmail.com>
2017-03-01 11:52:27 +0100

    drobne poprawki

--- Egzamin2/Test1ViewController.swift
+++ Egzamin2/Test1ViewController.swift
@@ -36,6 +36,7 @@ class Test1ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
