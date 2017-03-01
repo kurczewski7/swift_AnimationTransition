b3fdc3818656cfcb642d77bb790fa00ff6a3d6e0
kurczewski7 <slawomir.kurczewski@gmail.com>
2017-03-01 11:50:17 +0100

    Dodano wyniki

--- Egzamin2/Egzamin2/Wynik1ViewController.swift
+++ Egzamin2/Egzamin2/Wynik1ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  Test1ViewController.swift
+//  Wynik1ViewController.swift
 //  Egzamin2
 //
 //  Created by Slawek Kurczewski on 01.03.2017.
@@ -7,20 +7,14 @@
 //
 
 import UIKit
-//tablica krotek: tytuł, obrazek, petanie1, pytanie2, pytanie3, dobraOdpowiedź, uzyskanaOdpowiedź
-let testList: [(String, String, String, String,String, Int, Int)]=[
-    ("Jaki to most?", "most", " Most Rędzński "," Most Zwierzyniecki "," Most Grunwaldzki ",3,0),
-    ("W pobliżu czego  jest ten most?", "wrynku", " Placu Grunwaldzkiego "," Rynku "," Hali Ludowej ",2,0),
-    ("Czy ten most nazywa się", "redzinski", " Most Rędzński "," Most Sikorskiego "," Most Milenijny ",3,0)]
 
-class Test1ViewController: UIViewController {
-
+class Wynik1ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var image: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let currentIndex = 0
@@ -30,6 +24,10 @@ class Test1ViewController: UIViewController {
         button1.setTitle(testList[currentIndex].2, for: .normal)
         button2.setTitle(testList[currentIndex].3, for: .normal)
         button3.setTitle(testList[currentIndex].4, for: .normal)
+        
+        button2.backgroundColor=UIColor.red
+        button3.backgroundColor=UIColor.green
+
 
         // Do any additional setup after loading the view.
     }
--- Egzamin2/Egzamin2/Wynik2ViewController.swift
+++ Egzamin2/Egzamin2/Wynik2ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  Test1ViewController.swift
+//  Wynik2ViewController.swift
 //  Egzamin2
 //
 //  Created by Slawek Kurczewski on 01.03.2017.
@@ -7,29 +7,24 @@
 //
 
 import UIKit
-//tablica krotek: tytuł, obrazek, petanie1, pytanie2, pytanie3, dobraOdpowiedź, uzyskanaOdpowiedź
-let testList: [(String, String, String, String,String, Int, Int)]=[
-    ("Jaki to most?", "most", " Most Rędzński "," Most Zwierzyniecki "," Most Grunwaldzki ",3,0),
-    ("W pobliżu czego  jest ten most?", "wrynku", " Placu Grunwaldzkiego "," Rynku "," Hali Ludowej ",2,0),
-    ("Czy ten most nazywa się", "redzinski", " Most Rędzński "," Most Sikorskiego "," Most Milenijny ",3,0)]
 
-class Test1ViewController: UIViewController {
-
+class Wynik2ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var image: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let currentIndex = 0
+        let currentIndex = 1
         titleLabel.text=testList[currentIndex].0
         image.image = UIImage(named: testList[currentIndex].1)
         
         button1.setTitle(testList[currentIndex].2, for: .normal)
         button2.setTitle(testList[currentIndex].3, for: .normal)
         button3.setTitle(testList[currentIndex].4, for: .normal)
+
 
         // Do any additional setup after loading the view.
     }
