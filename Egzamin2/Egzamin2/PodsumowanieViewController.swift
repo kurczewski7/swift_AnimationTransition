6d1128d78e862a606c13d505a9adb49a0a4e622e
kurczewski7 <slawomir.kurczewski@gmail.com>
2017-03-02 00:22:01 +0100

    Poprawiono podsumowanie

--- Egzamin2/Egzamin2/PodsumowanieViewController.swift
+++ Egzamin2/Egzamin2/PodsumowanieViewController.swift
@@ -23,15 +23,43 @@ class PodsumowanieViewController: UIViewController {
         super.viewDidLoad()
         
 
+
+
+        
+        //var procent =  100 * div(Int32(iloscPoprawnych), Int32(testList.count))
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let textNotOK=": źle"
+        let textOK=": dobrze"
+        
         print("Rozmiar: \(testList.count)")
         var iloscPoprawnych:Int=0
+        
+        wynikTestu1.textColor=UIColor.red
+        wynikTestu2.textColor=UIColor.red
+        wynikTestu3.textColor=UIColor.red
+        
+        wynikTestu1.text="Test 1"+textNotOK
+        wynikTestu2.text="Test 2"+textNotOK
+        wynikTestu3.text="Test 3"+textNotOK
+        
         for i in 0 ..< testList.count
         {
             print("   i= \(i)")
-
+            
             if(testList[i].5 == testList[i].6){
                 iloscPoprawnych+=1
                 print("iloscPoprawnych: \(iloscPoprawnych)")
+                
+                switch i {
+                    case 0: wynikTestu1.textColor=UIColor.green; wynikTestu1.text="Test 1"+textOK
+                    case 1: wynikTestu2.textColor=UIColor.green; wynikTestu2.text="Test 2"+textOK
+                    case 2: wynikTestu3.textColor=UIColor.green; wynikTestu3.text="Test 3"+textOK
+                default:  break
+                }
             }
         }
         //iloscPoprawnych=1
@@ -42,21 +70,7 @@ class PodsumowanieViewController: UIViewController {
         
         procentPoprawnych.text=String(procent)
         progressBar.setProgress(Float(procent/100), animated: false)
-        //progressBar.setProgress(Float(procent), animated: false)
-        
-        wynikTestu1.text="Test 1: dobrze"
-        wynikTestu2.text="Test 2: źle"
-        
-        wynikTestu3.text="Test 3: dobrze"
-        wynikTestu3.textColor=UIColor.green
-
-
-        
-        //var procent =  100 * div(Int32(iloscPoprawnych), Int32(testList.count))
-        
-        // Do any additional setup after loading the view.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
